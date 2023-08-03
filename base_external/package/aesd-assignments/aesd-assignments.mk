de5b6f5d34ee9d5dc9355c636ff9751af990e6ca
sinanaltinsoy <altinsoy.snn@gmail.com>
2023-08-03 16:29:48 +0200

    change commit id

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 04e76919c9d93668a88a395d42620adfb36dc12b
+AESD_ASSIGNMENTS_VERSION = 2bc727cef66af65a6816349a0d990f272c41b4ec
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
